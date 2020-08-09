97051ec22db7dc3a9c95fef9469bdcf3488bdb44
ryotinjpn <ryotinjpn@gmail.com>
2020-08-09 19:13:58 +0900

    fix route delete comment

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/posts/{post}/edit', 'PostsController@edit');
 Route::patch('/posts/{post}', 'PostsController@update');
 Route::delete('/posts/{post}', 'PostsController@destroy');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
-Route::delete('/posts/{post}/comments/{/comment}', 'CommentsController@destroy');
+Route::delete('/posts/{post}/comments/{comment}', 'CommentsController@destroy');
